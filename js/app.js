03f32ff777b83607a82282d7f7fe86eaa5c17d5e
McKenna Penley <mckennapenley@gmail.com>
2021-09-14 20:15:49 -0400

    updates heights of elements to respond to resizing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
- randomWords = ["cheeseburger", "florida", "potato", "cat", "biology", "halloween", "banana", "space"];
+randomWords = [
+  "cheeseburger",
+  "florida",
+  "potato",
+  "cat",
+  "biology",
+  "halloween",
+  "banana",
+  "space",
+  "pizza",
+  "paris",
+];
 
 let randomWord;
 
@@ -13,11 +24,11 @@ function generateGame() {
   //get random num
   const randomNum = Math.floor(Math.random() * randomWords.length);
 
-  //use random num to pull search term 
+  //use random num to pull search term
   randomWord = randomWords[randomNum];
 
-//create random offset to pass into api call
-  const randomOffset = Math.floor(Math.random() * 100);
+  //create random offset to pass into api call
+  const randomOffset = Math.floor(Math.random() * 1000);
   //prevent default behavior of a form
   // event.preventDefault();
 
@@ -33,8 +44,6 @@ function generateGame() {
       console.log("bad request: ", error);
     }
   );
-
-
 }
 // handleGetData()
 
@@ -42,29 +51,29 @@ $("#start-btn").on("click", generateGame);
 
 function render(dataFromAJAX) {
   $("img").attr("src", "");
-  $giph1.attr("src", dataFromAJAX.data[0].images.fixed_width.url);
-  $giph2.attr("src", dataFromAJAX.data[1].images.fixed_width.url);
-  $giph3.attr("src", dataFromAJAX.data[2].images.fixed_width.url);
+  $giph1.attr("src", dataFromAJAX.data[0].images.fixed_height.url);
+  $giph2.attr("src", dataFromAJAX.data[1].images.fixed_height.url);
+  $giph3.attr("src", dataFromAJAX.data[2].images.fixed_height.url);
 }
-
 
 function getGuess(event) {
-    //prevent default behavior of a form
-    event.preventDefault();
-  
-    userInput = $input.val();
+  //prevent default behavior of a form
+  event.preventDefault();
 
+  userInput = $input.val();
 
-    if (userInput !== randomWord){
-    // $('main').css("background-color","red");
-    $("#guess-box").css("background-color", "red");
-    // $("#guess-box").css("background-color", "white");
-    } else if (userInput === randomWord){
-        $("#guess-box").css("background-color", "green");
-        setTimeout(function(){alert("You Win!")},100); 
-    }
+  if (userInput !== randomWord) {
+    $("#guess-box")
+      .fadeOut(100)
+      .fadeIn(100)
+      .fadeOut(100)
+      .fadeIn(100);
+  } else if (userInput === randomWord) {
+    $("#guess-box").css("background-color", "green");
+    setTimeout(function () {
+      alert("You Win!");
+    }, 100);
+  }
 }
 
-    $("form").on("submit", getGuess);
-
-
+$("form").on("submit", getGuess);
