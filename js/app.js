8701244f35c542039123d93a506204804220ef82
McKenna Penley <mckennapenley@gmail.com>
2021-09-19 17:50:02 -0400

    refactors manin game logic, adds play again button

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-randomWords = [
+const randomWords = [
   "cheeseburger",
   "florida",
   "potato",
@@ -42,10 +42,10 @@ randomWords = [
   "fuzzy",
   "beautiful",
   "sandy",
-  "DJ",
+  "dj",
   "explosion",
   "sparkle",
-  "model"
+  "model",
 ];
 
 let randomWord;
@@ -57,43 +57,33 @@ const $giph3 = $("#giph3");
 const $input = $("input[type=text]");
 let guessCount = 0;
 let guessesLeft = 5;
-let isWinner = false;
+let isGameOver = false;
 let isGameStarted = false;
 
-$('#gifs').hide()
+$("#gifs").hide();
+$("#play-again").hide();
 
-// const $input = $("input[type=text]");
 
 function generateGame() {
-  isWinner = false;
+  isGameOver = false;
   isGameStarted = true;
-  guessCount = 0;
+  $("#play-again").hide();
+
   guessesLeft = 5;
   $("#guess-count").text(`Guesses Left: 5`);
   $("#win-lose").text("");
 
-  //clear input box
   $input.val("");
-  //set input box color back to white
-  $("#guess-box").css("background-color", "white");
-  //get random num
   const randomNum = Math.floor(Math.random() * randomWords.length);
 
-  //use random num to pull search term
   randomWord = randomWords[randomNum];
 
-  //hide the answer
   $answer = $("#answer");
   $answer.hide();
-  // assign random word to the answer
+
   $answer.text(`Answer: ${randomWord}`);
 
-  //create random offset to pass into api call
   const randomOffset = Math.floor(Math.random() * 100);
-  //prevent default behavior of a form
-  // event.preventDefault();
-
-  // userInput = $input.val();
 
   $.ajax({
     url: `https://api.giphy.com/v1/gifs/search?api_key=G7tdOAm3RVKG0bSk2wKHNZ3Brpb99YBt&q=${randomWord}&limit=3&offset=${randomOffset}&rating=pg`,
@@ -105,71 +95,83 @@ function generateGame() {
       console.log("bad request: ", error);
     }
   );
-  $('#instructions').hide();
-  $('#gifs').show();
-  
 }
-// handleGetData()
-
-$("#start-btn").on("click", generateGame);
-
 
 function render(dataFromAJAX) {
+  
   $("img").attr("src", "");
+
+  
   $giph1.attr("src", dataFromAJAX.data[0].images.fixed_height.url);
   $giph2.attr("src", dataFromAJAX.data[1].images.fixed_height.url);
   $giph3.attr("src", dataFromAJAX.data[2].images.fixed_height.url);
+
+  $("#instructions").hide();
+  $("#gifs").show();
 }
 
 function getGuess(event) {
   //prevent default behavior of a form
   event.preventDefault();
-  userInput = $input.val().toLowerCase();
-  if (isWinner) {
+
+  if (isGameOver) {
     return;
   }
-  if (isGameStarted){
 
-  if (userInput !== "") {
-    if (guessesLeft > 0) {
-      guessCount++;
-      guessesLeft = 5 - guessCount;
+  if (isGameStarted) {
+    userInput = $input.val().toLowerCase();
+
+    if (userInput) {
+      guessesLeft--;
+
       $("#guess-count").text(`Guesses Left: ${guessesLeft}`);
-    }
 
-    if (guessesLeft > 0 && userInput !== randomWord) {
-      wrongAnswerResponse();
-    } else if (guessesLeft >= 0 && userInput === randomWord) {
-      winGame();
-    } else if (guessesLeft === 0 && userInput !== randomWord) {
-      loseGame();
+      interpretUserInput();
     }
   }
 }
+
+function giveUpResponse() {
+  if (!isGameStarted) {
+    return;
+  } else if (!isGameOver) {
+    loseGame();
+    guessesLeft = 0;
+    $("#guess-count").text(`Guesses Left: ${guessesLeft}`);
+  }
+};
+
+function interpretUserInput() {
+  if (userInput === randomWord) {
+    winGame();
+  }
+  if (guessesLeft > 0 && userInput !== randomWord) {
+    wrongAnswerResponse();
+  }
+  if (guessesLeft === 0 && userInput !== randomWord) {
+    loseGame();
+  }
 }
-
-$("form").on("submit", getGuess);
-
-$("#give-up-btn").click(function () {
-  if (!isWinner){
-  // $answer.show();
-  loseGame();
-  guessesLeft = 0;
-  $("#guess-count").text(`Guesses Left: ${guessesLeft}`);
-}});
 
 function wrongAnswerResponse() {
   $("#guess-box").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-
-  $("form").trigger("reset")
+  $("form").trigger("reset");
 }
 
 function loseGame() {
   $("#win-lose").text("Game Over - You Lose!");
   $answer.show();
+  isGameOver = true;
+  $("#play-again").show();
 }
 
 function winGame() {
-  isWinner = true;
   $("#win-lose").text("Winner!");
+  isGameOver = true;
+  $("#play-again").show();
 }
+
+$("#play-again").on("click", generateGame);
+$("form").on("submit", getGuess);
+$("#start-btn").on("click", generateGame);
+$("#give-up-btn").on("click", giveUpResponse);
